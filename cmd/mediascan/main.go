b955f23f32828f4df027f17dc8e1358ee74afa22
David Thorpe <djt@mutablelogic.com>
2019-08-11 18:29:43 +0100

    Added lots of metadata tags

--- cmd/mediascan/main.go
+++ cmd/mediascan/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 
 	// Frameworks
 	gopi "github.com/djthorpe/gopi"
@@ -13,19 +15,98 @@ import (
 	_ "github.com/djthorpe/gopi/sys/logger"
 )
 
-func Main(app *gopi.AppInstance, done chan<- struct{}) error {
+var (
+	paths = make(chan string)
+	files = make(chan string)
+)
 
-	// Get media object
-	media := app.ModuleInstance("ffmpeg").(media.Media)
-
-	// Get filenames
-	for _, filename := range app.AppFlags.Args() {
-		if file, err := media.Open(filename); err != nil {
-			app.Logger.Error("%v: %v", filename, err)
-		} else {
-			fmt.Println(file)
+func WalkFiles(app *gopi.AppInstance, start chan<- struct{}, stop <-chan struct{}) error {
+	// Get media object, start processing
+	ffmpeg := app.ModuleInstance("ffmpeg").(media.Media)
+	start <- gopi.DONE
+FOR_LOOP:
+	for {
+		select {
+		case filename := <-files:
+			// Ignore files by guessing their type
+			if ffmpeg.TypeFor(filename) == media.MEDIA_TYPE_NONE {
+				app.Logger.Warn("Ignoring %v", filename)
+			} else if file, err := ffmpeg.Open(filename); err != nil {
+				app.Logger.Error("%v: %v", filename, err)
+			} else {
+				fmt.Println(file)
+				ffmpeg.Destroy(file)
+			}
+		case <-stop:
+			break FOR_LOOP
 		}
 	}
+	return nil
+}
+
+func WalkPaths(app *gopi.AppInstance, start chan<- struct{}, stop <-chan struct{}) error {
+	recursive := true
+	start <- gopi.DONE
+FOR_LOOP:
+	for {
+		select {
+		case path := <-paths:
+			if err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+				// Always skip hidden files
+				if strings.HasPrefix(info.Name(), ".") {
+					return nil
+				}
+				// Return any errors
+				if err != nil {
+					return err
+				}
+				// Recurse into folders
+				if info.IsDir() && recursive == false {
+					return filepath.SkipDir
+				}
+				// Skip non-regular files
+				if info.Mode().IsRegular() == false {
+					return nil
+				}
+
+				// Ignore zero-sized files
+				if info.Size() == 0 {
+					return nil
+				}
+
+				// Output path for opening
+				files <- path
+
+				// Return success
+				return nil
+			}); err != nil {
+				app.Logger.Error("%v: %v", path, err)
+			}
+		case <-stop:
+			break FOR_LOOP
+		}
+	}
+	return nil
+}
+
+func Main(app *gopi.AppInstance, done chan<- struct{}) error {
+
+	// Get paths
+	for _, path := range app.AppFlags.Args() {
+		if stat, err := os.Stat(path); os.IsNotExist(err) {
+			app.Logger.Error("%v: Not Found", path)
+		} else if err != nil {
+			app.Logger.Error("%v: %v", path, err)
+		} else if stat.IsDir() {
+			paths <- path
+		} else if stat.Mode().IsRegular() {
+			files <- path
+		}
+	}
+
+	// Wait for CTRL+C
+	app.Logger.Info("Waiting for CTRL+C")
+	app.WaitForSignal()
 
 	// Success
 	done <- gopi.DONE
@@ -39,5 +120,5 @@ func main() {
 	config := gopi.NewAppConfig("ffmpeg")
 
 	// Run the command line tool
-	os.Exit(gopi.CommandLineTool2(config, Main))
+	os.Exit(gopi.CommandLineTool2(config, Main, WalkPaths, WalkFiles))
 }
--- sys/ffmpeg/ffmpeg.go
+++ sys/ffmpeg/ffmpeg.go
@@ -12,7 +12,10 @@ package ffmpeg
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"strconv"
+	"strings"
+	"time"
 
 	// Frameworks
 	gopi "github.com/djthorpe/gopi"
@@ -33,7 +36,9 @@ type ffmpeg struct {
 }
 
 type ffinput struct {
-	ctx *ff.AVFormatContext
+	log  gopi.Logger
+	ctx  *ff.AVFormatContext
+	keys map[media.MetadataKey]string
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -84,12 +89,9 @@ func (this *ffmpeg) Open(filename string) (media.MediaFile, error) {
 		return nil, err
 	} else if stat.Mode().IsRegular() == false {
 		return nil, gopi.ErrBadParameter
-	} else if ctx := ff.NewAVFormatContext(); ctx == nil {
-		return nil, gopi.ErrAppError
-	} else if err := ctx.OpenInput(filename, nil); err != nil {
+	} else if file, err := NewInput(filename, this.log); err != nil {
 		return nil, err
 	} else {
-		file := &ffinput{ctx: ctx}
 		this.files = append(this.files, file)
 		return file, nil
 	}
@@ -101,14 +103,73 @@ func (this *ffmpeg) Destroy(file media.MediaFile) error {
 	return gopi.ErrNotImplemented
 }
 
+func (this *ffmpeg) TypeFor(filename string) media.MediaType {
+	ext := strings.ToLower(filepath.Ext(filename))
+	switch ext {
+	case ".mp4", ".m4v", ".mov", ".m2v", ".vob":
+		return media.MEDIA_TYPE_MOVIE
+	case ".mp3", ".aac", ".m4a":
+		return media.MEDIA_TYPE_MUSIC
+	case ".m4r":
+		return media.MEDIA_TYPE_RINGTONE
+	default:
+		return media.MEDIA_TYPE_NONE
+	}
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // MEDIAFILE INTERFACE IMPLEMENTATION
+
+func NewInput(filename string, log gopi.Logger) (*ffinput, error) {
+	if stat, err := os.Stat(filename); os.IsNotExist(err) {
+		return nil, gopi.ErrNotFound
+	} else if err != nil {
+		return nil, err
+	} else if ctx := ff.NewAVFormatContext(); ctx == nil {
+		return nil, gopi.ErrAppError
+	} else if err := ctx.OpenInput(filename, nil); err != nil {
+		return nil, err
+	} else {
+		dict := ctx.Metadata()
+		this := new(ffinput)
+		this.log = log
+		this.ctx = ctx
+		this.keys = make(map[media.MetadataKey]string, dict.Count()+10)
+
+		// Set the file attributes
+		this.keys[media.METADATA_KEY_FILENAME] = filename
+		this.keys[media.METADATA_KEY_FILESIZE] = fmt.Sprint(stat.Size())
+		this.keys[media.METADATA_KEY_EXTENSION] = filepath.Ext(filename)
+		this.keys[media.METADATA_KEY_MODIFIED] = stat.ModTime().Format(time.RFC3339)
+
+		// Read the metadata
+		for _, entry := range dict.Entries() {
+			entry_key := entry.Key()
+			if strings.HasPrefix(entry_key, "iTun") || entry_key == "Encoding Params" {
+				// We ignore any iTunes-specific metadata
+				this.log.Debug2("Ignoring metadata entry: %v", entry)
+			} else if key := MetadataKeyFor(entry_key); key != media.METADATA_KEY_NONE {
+				this.keys[key] = entry.Value()
+			} else {
+				this.log.Warn("Ignoring metadata entry: %v", entry)
+			}
+		}
+
+		return this, nil
+	}
+}
 
 func (this *ffinput) String() string {
 	if this.ctx == nil {
 		return fmt.Sprintf("<ffinput>{ ctx=nil }")
 	} else {
-		return fmt.Sprintf("<ffinput>{ filename=%v }", strconv.Quote(this.Filename()))
+		metadata := ""
+		for k, v := range this.keys {
+			metadata_key := strings.ToLower(strings.TrimPrefix(fmt.Sprint(k), "METADATA_KEY_"))
+			metadata_value := strconv.Quote(v)
+			metadata += fmt.Sprintf("%v=%v ", metadata_key, metadata_value)
+		}
+		return fmt.Sprintf("<ffinput>{ filename=%v metadata={%v} }", strconv.Quote(this.Filename()), strings.TrimSpace(metadata))
 	}
 }
 
@@ -121,11 +182,118 @@ func (this *ffinput) Filename() string {
 }
 
 func (this *ffinput) Destroy() error {
+	this.log.Debug2("<ffinput.Destroy>{ ctx=%v }", this.ctx)
+
 	if this.ctx == nil {
-		return gopi.ErrAppError
+		// Do nothing
+		return nil
 	} else {
 		this.ctx.Close()
 		this.ctx = nil
+		this.keys = nil
 		return nil
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// MEDIAITEM INTERFACE IMPLEMENTATION
+
+func (this *ffinput) Keys() []media.MetadataKey {
+	if this.ctx == nil {
+		return nil
+	} else {
+		return nil
+	}
+}
+
+func (this *ffinput) StringForKey(media.MetadataKey) string {
+	return ""
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// CONVERT FFMPEG KEYS
+
+func MetadataKeyFor(key string) media.MetadataKey {
+	switch key {
+	case "major_brand":
+		return media.METADATA_KEY_BRAND_MAJOR
+	case "compatible_brands":
+		return media.METADATA_KEY_BRAND_COMPATIBLE
+	case "creation_time":
+		return media.METADATA_KEY_CREATED
+	case "encoder":
+		return media.METADATA_KEY_ENCODER
+	case "album":
+		return media.METADATA_KEY_ALBUM
+	case "album_artist":
+		return media.METADATA_KEY_ALBUM_ARTIST
+	case "artist":
+		return media.METADATA_KEY_ARTIST
+	case "comment":
+		return media.METADATA_KEY_COMMENT
+	case "composer":
+		return media.METADATA_KEY_COMPOSER
+	case "copyright":
+		return media.METADATA_KEY_COPYRIGHT
+	case "date":
+		return media.METADATA_KEY_YEAR
+	case "disc":
+		return media.METADATA_KEY_DISC
+	case "encoded_by":
+		return media.METADATA_KEY_ENCODED_BY
+	case "filename":
+		return media.METADATA_KEY_FILENAME
+	case "genre":
+		return media.METADATA_KEY_GENRE
+	case "language":
+		return media.METADATA_KEY_LANGUAGE
+	case "performer":
+		return media.METADATA_KEY_PERFORMER
+	case "publisher":
+		return media.METADATA_KEY_PUBLISHER
+	case "service_name":
+		return media.METADATA_KEY_SERVICE_NAME
+	case "service_provider":
+		return media.METADATA_KEY_SERVICE_PROVIDER
+	case "title":
+		return media.METADATA_KEY_TITLE
+	case "track":
+		return media.METADATA_KEY_TRACK
+	case "major_version":
+		return media.METADATA_KEY_VERSION_MAJOR
+	case "minor_version":
+		return media.METADATA_KEY_VERSION_MINOR
+	case "show":
+		return media.METADATA_KEY_SHOW
+	case "season_number":
+		return media.METADATA_KEY_SEASON
+	case "episode_sort":
+		return media.METADATA_KEY_EPISODE_SORT
+	case "episode_id":
+		return media.METADATA_KEY_EPISODE_ID
+	case "compilation":
+		return media.METADATA_KEY_COMPILATION
+	case "gapless_playback":
+		return media.METADATA_KEY_GAPLESS_PLAYBACK
+	case "account_id":
+		return media.METADATA_KEY_ACCOUNT_ID
+	case "description":
+		return media.METADATA_KEY_DESCRIPTION
+	case "media_type":
+		return media.METADATA_KEY_MEDIA_TYPE
+	case "purchase_date":
+		return media.METADATA_KEY_PURCHASED
+	case "sort_album":
+		return media.METADATA_KEY_ALBUM_SORT
+	case "sort_artist":
+		return media.METADATA_KEY_ARTIST_SORT
+	case "sort_name":
+		return media.METADATA_KEY_TITLE_SORT
+	case "synopsis":
+		return media.METADATA_KEY_SYNOPSIS
+	case "grouping":
+		return media.METADATA_KEY_GROUPING
+	default:
+		return media.METADATA_KEY_NONE
 	}
 }
