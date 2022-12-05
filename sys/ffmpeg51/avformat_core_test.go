ca260496b86a08834f7272d3e8eaf3e443121757
David Thorpe <djt@mutablelogic.com>
2022-12-05 21:14:18 +0100

    Added new files for ffmpeg51

---
+++ sys/ffmpeg51/avformat_core_test.go
@@ -0,0 +1,45 @@
+package ffmpeg_test
+
+import (
+	"testing"
+
+	// Pacakge imports
+	ffmpeg "github.com/mutablelogic/go-media/sys/ffmpeg51"
+)
+
+////////////////////////////////////////////////////////////////////////////////
+// TESTS
+
+func Test_avformat_000(t *testing.T) {
+	t.Log("avformat_version=", ffmpeg.AVFormat_version())
+}
+
+func Test_avformat_001(t *testing.T) {
+	t.Log("avformat_configuration=", ffmpeg.AVFormat_configuration())
+}
+
+func Test_avformat_002(t *testing.T) {
+	t.Log("avformat_license=", ffmpeg.AVFormat_license())
+}
+
+func Test_avformat_003(t *testing.T) {
+	var opaque uintptr
+	for {
+		format := ffmpeg.AVFormat_av_muxer_iterate(&opaque)
+		if format == nil {
+			break
+		}
+		t.Log("muxer=", format)
+	}
+}
+
+func Test_avformat_003(t *testing.T) {
+	var opaque uintptr
+	for {
+		format := ffmpeg.AVFormat_av_demuxer_iterate(&opaque)
+		if format == nil {
+			break
+		}
+		t.Log("demuxer=", format)
+	}
+}
