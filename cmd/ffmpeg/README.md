41fd3757e716eb5b21d96e0f138c5bf04140b0af
David Thorpe <djt@mutablelogic.com>
2024-06-12 13:42:27 +0200

    Added show_metadata example

--- cmd/ffmpeg/README.md
+++ cmd/ffmpeg/README.md
@@ -6,3 +6,4 @@ using the low-level golang bindings.
 
 * [remux](remux) - Remuxing - libavformat/libavcodec demuxing and muxing API usage example. Remux streams from one container format to another. Data is copied from the input to the output without transcoding.
 * [scale_video](scale_video) - libswscale API usage example. Generate a synthetic video signal and use libswscale to perform rescaling.
+* [show_metadata](show_metadata) - libavformat metadata extraction API usage example. Show metadata from an input file.
---
+++ cmd/ffmpeg/show_metadata/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	// Packages
+	ff "github.com/mutablelogic/go-media/sys/ffmpeg61"
+)
+
+func main() {
+	in := flag.String("in", "", "input file")
+	flag.Parse()
+
+	// Check out and size
+	if *in == "" {
+		log.Fatal("-in argument must be specified")
+	}
+
+	// Open input file
+	input, err := ff.AVFormat_open_url(*in, nil, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ff.AVFormat_close_input(input)
+
+	if err := ff.AVFormat_find_stream_info(input, nil); err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		tag := AVUtil_dict_iterate(input.Metadata(), tag)
+		if tag != nil {
+			log.Println(tag, Key(), "=>", tag.Value())
+		} else {
+			break
+		}
+	}
+}
