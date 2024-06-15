e5d84ae6f4cbb67924866ccf95b2c7da5073094a
David Thorpe <djt@mutablelogic.com>
2024-06-15 08:54:21 +0200

    Added decode_audio example

--- cmd/ffmpeg/decode_audio/main.go
+++ cmd/ffmpeg/decode_audio/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"encoding/binary"
 	"errors"
 	"flag"
-	"fmt"
 	"io"
 	"log"
 	"os"
 	"syscall"
+	"unsafe"
 
 	// Packages
 	ff "github.com/mutablelogic/go-media/sys/ffmpeg61"
@@ -18,9 +19,24 @@ const (
 	AUDIO_REFILL_THRESH = 4096
 )
 
+// NativeEndian is the ByteOrder of the current system.
+var NativeEndian binary.ByteOrder
+
+func init() {
+	// Examine the memory layout of an int16 to determine system
+	// endianness.
+	var one int16 = 1
+	b := (*byte)(unsafe.Pointer(&one))
+	if *b == 0 {
+		NativeEndian = binary.BigEndian
+	} else {
+		NativeEndian = binary.LittleEndian
+	}
+}
+
 func main() {
 	in := flag.String("in", "", "input file")
-	codec_name := flag.String("codec", "mp3", "codec to use")
+	codec_name := flag.String("codec", "mp3", "input codec to use")
 	out := flag.String("out", "", "output file")
 	flag.Parse()
 
@@ -58,12 +74,17 @@ func main() {
 	}
 	defer r.Close()
 
+	log.Print("in file=", r.Name())
+	log.Print("  codec=", codec)
+
 	// open file for writing
 	w, err := os.Create(*out)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer w.Close()
+
+	log.Print("out file=", w.Name())
 
 	// Create a packet
 	packet := ff.AVCodec_packet_alloc()
@@ -85,17 +106,22 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	for {
-		fmt.Println("data_size", data_size)
+		log.Print("in data_size=", data_size)
 		if data_size == 0 {
 			break
 		}
 
 		// Parse the data
-		size := ff.AVCodec_parser_parse(parser, ctx, packet, inbuf[:data_size], ff.AV_NOPTS_VALUE, ff.AV_NOPTS_VALUE, 0)
+		size := ff.AVCodec_parser_parse(parser, ctx, packet, inbuf, ff.AV_NOPTS_VALUE, ff.AV_NOPTS_VALUE, 0)
 		if size < 0 {
 			log.Fatal("Error while parsing")
 		}
+		log.Print("parsed bytes=", size)
+		log.Print("packet to decode size=", packet.Size())
+
+		// Adjust the input buffer beyond the parsed data
 		inbuf = inbuf[size:]
 		data_size -= size
 
@@ -105,24 +131,59 @@ func main() {
 				log.Fatal(err)
 			}
 		}
+
+		// TODO
+		/*
+					if (data_size < AUDIO_REFILL_THRESH) {
+			            memmove(inbuf, data, data_size);
+			            data = inbuf;
+			            len = fread(data + data_size, 1,
+			                        AUDIO_INBUF_SIZE - data_size, f);
+			            if (len > 0)
+			                data_size += len;
+			        }*/
 	}
 
 	// Flush the decoder
-	ff.AVCodec_packet_empty(packet)
+	ff.AVCodec_packet_unref(packet)
 	if err := decode(w, ctx, packet, frame); err != nil {
 		log.Fatal(err)
 	}
+
+	// Print output pcm infomations, because there have no metadata of pcm
+	sfmt := ctx.SampleFormat()
+	if ff.AVUtil_sample_fmt_is_planar(sfmt) {
+		packed := ff.AVUtil_get_sample_fmt_name(sfmt)
+		log.Printf("Warning: the sample format the decoder produced is planar (%s). This example will output the first channel only.\n", packed)
+		sfmt = ff.AVUtil_get_packed_sample_fmt(ctx.SampleFormat())
+	}
+
+	n_channels := ctx.ChannelLayout().NumChannels()
+	fmt, err := get_format_from_sample_fmt(sfmt)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Print output pcm infomations
+	log.Printf("Play the output audio file with the command:\n  ffplay -f %s -ac %d -ar %d %s\n", fmt, n_channels, ctx.SampleRate(), *out)
 }
 
 func decode(w io.Writer, ctx *ff.AVCodecContext, packet *ff.AVPacket, frame *ff.AVFrame) error {
+	// bytes per sample
+	bytes_per_sample := ff.AVUtil_get_bytes_per_sample(ctx.SampleFormat())
+	if bytes_per_sample < 0 {
+		return errors.New("failed to calculate bytes per sample")
+	}
+
 	// send the packet with the compressed data to the decoder
+	log.Print("decode packet bytes=", packet.Size())
 	if err := ff.AVCodec_send_packet(ctx, packet); err != nil {
 		return err
 	}
 
 	// Read all the output frames (in general there may be any number of them)
 	for {
-		log.Println("  receive_packet")
+		log.Println("  receive_frame")
 		if err := ff.AVCodec_receive_frame(ctx, frame); errors.Is(err, syscall.EAGAIN) || errors.Is(err, io.EOF) {
 			return nil
 		} else if err != nil {
@@ -130,16 +191,41 @@ func decode(w io.Writer, ctx *ff.AVCodecContext, packet *ff.AVPacket, frame *ff.
 			return err
 		}
 
-		data_size := ff.AVUtil_get_bytes_per_sample(ctx.SampleFormat())
-		if data_size < 0 {
-			log.Fatal("Failed to calculate data size")
-		}
-
+		log.Println("  write frame bytes=", frame.NumSamples()*bytes_per_sample*ctx.ChannelLayout().NumChannels())
 		for i := 0; i < frame.NumSamples(); i++ {
 			for ch := 0; ch < ctx.ChannelLayout().NumChannels(); ch++ {
-				buf := frame.Bytes(ch)
-				_, err := w.Write(buf[i*data_size : data_size])
+				buf := frame.Uint8(ch)
+				_, err := w.Write(buf[i*bytes_per_sample : (i+1)*bytes_per_sample])
+				if err != nil {
+					return err
+				}
 			}
 		}
 	}
+}
+
+func get_format_from_sample_fmt(sample_fmt ff.AVSampleFormat) (string, error) {
+	type sample_fmt_entry struct {
+		sample_fmt ff.AVSampleFormat
+		fmt_be     string
+		fmt_le     string
+	}
+	sample_fmt_entries := []sample_fmt_entry{
+		{ff.AV_SAMPLE_FMT_U8, "u8", "u8"},
+		{ff.AV_SAMPLE_FMT_S16, "s16be", "s16le"},
+		{ff.AV_SAMPLE_FMT_S32, "s32be", "s32le"},
+		{ff.AV_SAMPLE_FMT_FLT, "f32be", "f32le"},
+		{ff.AV_SAMPLE_FMT_DBL, "f64be", "f64le"},
+	}
+
+	for _, entry := range sample_fmt_entries {
+		if sample_fmt == entry.sample_fmt {
+			if NativeEndian == binary.LittleEndian {
+				return entry.fmt_le, nil
+			} else {
+				return entry.fmt_be, nil
+			}
+		}
+	}
+	return "", errors.New("sample format is not supported as output format")
 }
--- sys/ffmpeg61/avutil_samplefmt.go
+++ sys/ffmpeg61/avutil_samplefmt.go
@@ -58,6 +58,18 @@ func AVUtil_get_bytes_per_sample(sample_fmt AVSampleFormat) int {
 	return int(C.av_get_bytes_per_sample(C.enum_AVSampleFormat(sample_fmt)))
 }
 
+// Get the required buffer size for the given audio parameters.
+// Returns the calculated buffer size and stride
+func AVUtil_get_buffer_size(nb_channels int, nb_samples int, sample_fmt AVSampleFormat) (int, int, error) {
+	var linesize C.int
+	ret := int(C.av_samples_get_buffer_size(&linesize, C.int(nb_channels), C.int(nb_samples), C.enum_AVSampleFormat(sample_fmt), 0))
+	if ret < 0 {
+		return 0, 0, AVError(ret)
+	} else {
+		return ret, int(linesize), nil
+	}
+}
+
 // Check if the sample format is planar.
 func AVUtil_sample_fmt_is_planar(sample_fmt AVSampleFormat) bool {
 	return C.av_sample_fmt_is_planar(C.enum_AVSampleFormat(sample_fmt)) != 0
@@ -67,4 +79,16 @@ func AVUtil_sample_fmt_is_planar(sample_fmt AVSampleFormat) bool {
 // include the number of samples in a single channel.
 func AVUtil_samples_get_buffer_size(sample_fmt AVSampleFormat, nb_channels int, nb_samples int, align bool) int {
 	return int(C.av_samples_get_buffer_size(nil, C.int(nb_channels), C.int(nb_samples), C.enum_AVSampleFormat(sample_fmt), boolToInt(align)))
+}
+
+// Get the packed alternative form of the given sample format.
+// If the passed sample_fmt is already in packed format, the format returned is the same as the input.
+func AVUtil_get_packed_sample_fmt(sample_fmt AVSampleFormat) AVSampleFormat {
+	return AVSampleFormat(C.av_get_packed_sample_fmt(C.enum_AVSampleFormat(sample_fmt)))
+}
+
+// Get the planar alternative form of the given sample format.
+// If the passed sample_fmt is already in planar format, the format returned is the same as the input.
+func AVUtil_get_planar_sample_fmt(sample_fmt AVSampleFormat) AVSampleFormat {
+	return AVSampleFormat(C.av_get_planar_sample_fmt(C.enum_AVSampleFormat(sample_fmt)))
 }
