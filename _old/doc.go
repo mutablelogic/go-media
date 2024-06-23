62cb84b6715e4400e6403c476c768965d1f7ef35
David Thorpe <djt@mutablelogic.com>
2024-06-23 19:34:45 +0200

    Cleanup _old

--- _old/doc.go
+++
@@ -1,4 +0,0 @@
-/* Media services for golang, including bindings for ffmpeg, command line application
-   and backend REST API for extraction, multiplexing and transcoding.
-*/
-package media
--- _old/pkg/_old/_media/doc.go
+++
@@ -1,5 +0,0 @@
-/*
-Package media provides high-level media services for multiplexing, extracting and transcoding audio
-and video.
-*/
-package media
