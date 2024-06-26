3b9c6dbb19f889fa08e8eb607919b5a0974897c7
David Thorpe <djt@mutablelogic.com>
2024-06-26 09:56:07 +0200

    Updated tests

--- manager_test.go
+++ manager_test.go
@@ -108,22 +108,3 @@ func Test_manager_007(t *testing.T) {
 
 	tablewriter.New(os.Stderr, tablewriter.OptHeader(), tablewriter.OptOutputText()).Write(codecs)
 }
-
-func Test_manager_008(t *testing.T) {
-	assert := assert.New(t)
-
-	manager, err := NewManager()
-	if !assert.NoError(err) {
-		t.SkipNow()
-	}
-
-	formats := manager.InputFormats(ANY)
-	assert.NotNil(formats)
-	for _, format := range formats {
-		if format.Type().Is(DEVICE) {
-			devices := manager.Devices(format)
-			assert.NotNil(devices)
-			t.Log(format, devices)
-		}
-	}
-}
